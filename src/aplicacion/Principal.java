0e10b882e756144888c3ccc3b6de85289d4d98c6
AlejandroLilloRodriguez <alejandro.lillorodriguez@usp.ceu.es>
2023-02-16 13:37:10 +0100

    Ejercicio4 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,9 @@
-package aplicacion;public class Principal {
+package aplicacion;
+
+import static dominio.Ejercicio4.suma;
+public class Principal {
+    public static void main (String [] args){
+        int[] lista={1,3,5,65,4,9,223,98,532,23};
+        System.out.print(suma(lista,lista.length-1));
+    }
 }
--- src/dominio/Ejercicio4.java
+++ src/dominio/Ejercicio4.java
@@ -1,2 +1,11 @@
-package dominio;public class Ejercicio4 {
+package dominio;
+
+public class Ejercicio4 {
+    public static int suma (int []lista,int i){
+        if(i==0)
+            return lista[0];
+        else
+            return lista[i]+suma(lista,i-1);
+
+    }
 }
